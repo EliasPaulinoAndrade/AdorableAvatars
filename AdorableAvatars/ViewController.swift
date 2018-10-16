a3d9ae1c1213a17ab01aae0b416e931c8c8742a0
Elias Paulino <eliaspaulinoandrade@gmail.com>
2018-10-16 08:46:59 -0300

    Initial Commit

---
+++ AdorableAvatars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdorableAvatars
+//
+//  Created by Elias Paulino on 16/10/18.
+//  Copyright © 2018 Elias Paulino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AdorableAvatarsTests/AdorableAvatarsTests.swift
@@ -0,0 +1,34 @@
+//
+//  AdorableAvatarsTests.swift
+//  AdorableAvatarsTests
+//
+//  Created by Elias Paulino on 16/10/18.
+//  Copyright © 2018 Elias Paulino. All rights reserved.
+//
+
+import XCTest
+@testable import AdorableAvatars
+
+class AdorableAvatarsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
