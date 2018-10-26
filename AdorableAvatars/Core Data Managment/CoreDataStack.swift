d266ea464b228a4b2d2e4d74fe60a16be1f6a6ec
Elias Paulino <eliaspaulinoandrade@gmail.com>
2018-10-26 19:12:45 -0300

    trying setup coredata

--- AdorableAvatars/Core Data Managment/CoreDataStack.swift
+++ AdorableAvatars/Core Data Managment/CoreDataStack.swift
@@ -10,14 +10,36 @@ import UIKit
 import CoreData
 
 class CoreDataStack {
+    
+    static var adorableAvatarsGroupUrl : URL? {
+        get {
+            let adorableAvatarsGroupUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.eliaspaulino.adorableavatars")
+            if let coreDataFile = adorableAvatarsGroupUrl?.appendingPathComponent("AdorableAvatars") {
+                
+                return coreDataFile
+            }
+
+            return adorableAvatarsGroupUrl
+        }
+    }
+    
     static var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "AdorableAvatars")
+        
+        if let coreURL = CoreDataStack.adorableAvatarsGroupUrl {
+            
+            let description = NSPersistentStoreDescription.init(url: coreURL)
+            description.shouldInferMappingModelAutomatically = true
+            description.shouldMigrateStoreAutomatically = true
+            container.persistentStoreDescriptions = [description]
+        }
+        
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
-        
         
         
         return container
